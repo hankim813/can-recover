0185f08b2006ed2cc7423a3ebf41447440758510
Han Kim <hankim813@gmail.com>
2014-06-23 22:38:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CanRecover::Application.config.secret_key_base = 'b3072042d0355dfd50984b2395feaa875b87f1a618ad6ddc28888c93932738565e62438290ff46feef04153d10ceff4d08215122f3c102ba2675ead0b7670340'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CanRecover::Application.config.session_store :cookie_store, key: '_can-recover_session'
