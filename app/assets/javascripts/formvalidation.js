34e946652b0c9801d058da0c56da78b5685d4938
Han Kim <hankim813@gmail.com>
2014-07-03 15:28:24 -0400

    Contact Page

---
+++ app/assets/javascripts/formvalidation.js
@@ -0,0 +1,27 @@
+function processForm()
+{	
+	var myform=document.getElementById("form1");
+	if (myform.fname.value=="")
+	{
+    alert("Name is required");
+    myform.fname.focus();
+    return;
+	}
+	else if (myform.femail.value=="")
+	{
+		alert("Email is required");
+		myform.femail.focus();
+		return;
+	}
+
+	else if (myform.fmessage.value="")
+	
+		alert("Message is required");
+		myform.fmessage.focus();
+		return;
+	
+	else
+	{
+		myform.submit();
+	}
+}
