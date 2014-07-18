dd313c926b88e65f60a0940ab7907038a1df5379
Han Kim <hankim813@gmail.com>
2014-07-18 10:58:17 -0400

    V2 Complete

--- app/assets/javascripts/formvalidation.js
+++ app/assets/javascripts/formvalidation.js
@@ -3,20 +3,20 @@ function processForm()
 	var myform=document.getElementById("form1");
 	if (myform.fname.value=="")
 	{
-    alert("Name is required");
+		$("#the-name").show("fast").delay(4000).hide("slow")
     myform.fname.focus();
     return;
 	}
 	else if (myform.femail.value=="")
 	{
-		alert("Email is required");
+		$("#the-email").show("fast").delay(4000).hide("slow")
 		myform.femail.focus();
 		return;
 	}
 
 	else if (myform.fmessage.value=="")
 	{
-		alert("Message is required");
+		$("#the-message").show("fast").delay(4000).hide("slow")
 		myform.fmessage.focus();
 		return;
 	}
